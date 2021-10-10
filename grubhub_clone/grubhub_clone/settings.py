b767ee60844bc90907d7c2c1e938d2aec962c70e
Ali Daley <ali.daley72@gmail.com>
2021-10-10 15:02:40 -0400

    Database created

--- grubhub_clone/grubhub_clone/settings.py
+++ grubhub_clone/grubhub_clone/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qehpy4=6d=kxl6ywhn15ja)f+-a9$sx*lkf&g4%hu$l4ca%l@a'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -123,3 +120,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+try:
+    from grubhub_clone.local_settings import *
+except ImportError:
+    pass
